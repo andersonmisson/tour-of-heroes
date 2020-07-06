e16504b5a6654639ea50e2de0b2d2e4a18fa817b
Anderson Misson <missondeveloper@gmail.com>
2020-07-06 16:29:51 -0300

    Parte 4 - Inject o HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,9 +14,10 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
+    
   }
 
   onSelect(hero: Hero): void {
