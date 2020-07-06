58512fc943dc2299e008e3bf23bbad1cf8593e87
Anderson Misson <missondeveloper@gmail.com>
2020-07-06 11:15:25 -0300

    Parte 2 - Declarando os novos heróis

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,6 +13,9 @@ export class HeroesComponent implements OnInit {
   /*
   hero = 'Windstorm';
   */
+
+ heroes = HEROES;
+
  hero: Hero = {
   id: 1,
   name: 'Windstorm'
